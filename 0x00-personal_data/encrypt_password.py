e5f7b4e2f865dc9e0546206fb4b83db6f3db5985
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-30 12:09:48 +0300

    Task 7

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,8 @@ def hash_password(password: str) -> bytes:
     """function that takes a password string
     and returns a hashed password string"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """function that expects 2 arguments and returns a boolean"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
