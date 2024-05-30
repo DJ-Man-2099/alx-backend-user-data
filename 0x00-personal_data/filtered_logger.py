736223bd39d8c5bd893ee925194667048a909876
DJ-Man-2099 <davidmesak@hotmail.com>
2024-05-30 10:35:10 +0300

    Task 1

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""First Task"""
+
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """function that returns the log message obfuscated"""
+    for field in fields:
+        # must be non greedy, to stop at first separator
+        regex = r"{}=.*?{}".format(field, separator)
+        message = re.sub(regex,
+                         f"{field}={redaction}{separator}",
+                         message)
+    return message
