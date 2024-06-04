ee2c0dddef29e32ea754dad76eb76dfab3a2908f
DJ-Man-2099 <davidmesak@hotmail.com>
2024-06-04 16:19:55 +0300

    Task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,6 +7,16 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ Basic Auth Class """
 
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ returns the Base64 part of the Authorization header
+        for a Basic Authentication """
+        if not (authorization_header
+                and isinstance(authorization_header, str)
+                and authorization_header.startswith("Basic ")):
+            return None
+        return authorization_header[6:]
+
     # def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
     #     """ returns False """
     #     if path and path[-1] != "/":
