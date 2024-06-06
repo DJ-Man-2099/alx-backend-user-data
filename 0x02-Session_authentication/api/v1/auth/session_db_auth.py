725782d07d456d779b627e34ccfbb9fa5470875d
DJ-Man-2099 <davidmesak@hotmail.com>
2024-06-06 18:12:08 +0300

    Correct????!!

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,68 +1,64 @@
 #!/usr/bin/env python3
-""" Session Authentication Expiry DB Management Class """
 
-from datetime import datetime, timedelta
-from os import getenv
-import uuid
+"""
+SessionDBAuth module
+"""
+
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from os import getenv
 from models.user_session import UserSession
+from datetime import datetime, timedelta
 
 
 class SessionDBAuth(SessionExpAuth):
-    """ Session Exp Auth Class """
+    """
+    SessionDBAuth class.
+    """
 
     def create_session(self, user_id=None):
-        """ that creates and stores new instance of UserSession
-        and returns the Session ID"""
-        session_id = super().create_session(user_id)
-
-        if session_id is None:
-            return None
-
-        kwargs = {
-                'user_id': user_id,
-                'session_id': session_id,
-            }
-        user_session = UserSession(**kwargs)
-
-        user_session.save()
-
-        return session_id
+        """
+        create_session.
+        """
+        if user_id:
+            session_id = super().create_session(user_id)
+            if not session_id:
+                return
+            new_user = UserSession(user_id=user_id, session_id=session_id)
+            new_user.save()
+            return session_id
 
     def user_id_for_session_id(self, session_id=None):
-        """that returns the User ID
-        by requesting UserSession in the database based on session_id"""
-
-        if session_id is None:
-            return None
-
+        """
+        user_id_for_session_id.
+        """
+        if not session_id:
+            return
         try:
-            user_sessions = UserSession.search({"session_id": session_id})
+            us_list = UserSession.search({session_id: session_id})
+            for us in us_list:
+                created_at = us.get('created_at', None)
+                if not created_at:
+                    return
+                if (datetime.now() > created_at +
+                        timedelta(seconds=self.session_duration)):
+                    return
+                return us.get('user_id', None)
         except Exception:
-            return None
-        exp_user_id = super().user_id_for_session_id(session_id)
-        if not user_sessions:
-            return None
-        if exp_user_id and exp_user_id == user_sessions[0].user_id:
-            return user_sessions[0].user_id
+            return
 
-        user_sessions[0].remove()
-        return None
-
-    def destroy_session(self, request=None):
-        """ that destroys the UserSession
-        based on the Session ID from the request cookie """
-        session_id = super().session_cookie(request)
-
-        if not super().destroy_session(request):
-            return False
-
-        try:
-            user_sessions = UserSession.search({"session_id": session_id})
-        except Exception:
-            return False
-
-        if not user_sessions:
-            return False
-        user_sessions[0].remove()
-        return True
+    def destroy_session(self, request=None) -> bool:
+        """
+        destroy_session.
+        """
+        if request:
+            session_id = self.session_cookie(request)
+            if session_id:
+                if super().destroy_session(request):
+                    try:
+                        us_list = UserSession.search({session_id: session_id})
+                        for us in us_list:
+                            us.remove()
+                            return True
+                    except Exception:
+                        return False
+        return False
