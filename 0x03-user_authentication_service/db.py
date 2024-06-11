645f298ff1bee5e212e768b7ac4e4337384d4581
DJ-Man-2099 <davidmesak@hotmail.com>
2024-06-12 00:10:13 +0300

    This??!

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -32,7 +34,7 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """add a new user to the db"""
         if not email or not hashed_password:
-            return None
+            raise InvalidRequestError
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
