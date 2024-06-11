628b144b14256e4e98ab66767dd4a9e10883aa40
DJ-Man-2099 <davidmesak@hotmail.com>
2024-06-11 23:57:41 +0300

    Correct Answer???

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,10 +30,13 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ save the user to the database """
-        if not (email and hashed_password):
-            return None
+        """
+        add_user.
+        """
+        if not email or not hashed_password:
+            return
         user = User(email=email, hashed_password=hashed_password)
-        self._session.add(user)
-        self._session.commit()
+        session = self._session
+        session.add(user)
+        session.commit()
         return user
