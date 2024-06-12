e85d70f998c5cf092ee7825b34a547f73c841d19
DJ-Man-2099 <davidmesak@hotmail.com>
2024-06-12 13:39:51 +0300

    Task 19

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -102,3 +102,21 @@ class Auth:
         self._db.session.commit()
 
         return reset_token
+
+    def update_password(self, reset_token: str, password: str):
+        """ Use the reset_token to find the corresponding user.
+        If it does not exist, raise a ValueError exception.
+        Otherwise, hash the password
+        and update the user’s hashed_password field
+        with the new hashed password
+        and the reset_token field to None. """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+
+        new_hashed_password = _hash_password(password)
+
+        self._db.update_user(user.id,
+                             hashed_password=new_hashed_password,
+                             reset_token=None)
