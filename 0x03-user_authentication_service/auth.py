a353ebad848b01adbc4622f1c7c13ae2d15d6a21
DJ-Man-2099 <davidmesak@hotmail.com>
2024-06-12 13:09:58 +0300

    Task 14

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -75,3 +75,13 @@ class Auth:
             return None
 
         return user
+
+    def destroy_session(self, user_id: int):
+        """  updates the corresponding user’s session ID to None. """
+        if user_id is None:
+            return None
+
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except Exception:
+            return None
