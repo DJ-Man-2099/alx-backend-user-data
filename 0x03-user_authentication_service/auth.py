7861d2e97876136862a5a29a7d33c99872e8a256
DJ-Man-2099 <davidmesak@hotmail.com>
2024-06-12 13:29:28 +0300

    Task 17

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -85,3 +85,20 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
         except NoResultFound:
             raise
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Find the user corresponding to the email.
+        If the user does not exist, raise a ValueError exception.
+        If it exists, generate a UUID
+        and update the user’s reset_token database field.
+        Return the token. """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+
+        reset_token = _generate_uuid()
+        user.reset_token = reset_token
+        self._db.session.commit()
+
+        return reset_token
